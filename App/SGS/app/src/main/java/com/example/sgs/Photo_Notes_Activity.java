75077d3606be759e9dafa0c443ea18e975be7595
Samy2206 <sanketrajuadhav@gmail.com>
2024-01-28 19:13:37 +0530

    photo notes module is added

---
+++ App/SGS/app/src/main/java/com/example/sgs/Photo_Notes_Activity.java
@@ -0,0 +1,14 @@
+package com.example.sgs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Photo_Notes_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_notes);
+    }
+}
